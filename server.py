4117196a4e6a4b2d2b05922a6345fe1f07d23d86
unicorn <118615352+ZixinP@users.noreply.github.com>
2024-01-30 20:08:55 +0100

    Update server.py

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ import time
 def main():
     HOST = '127.0.0.1'
     PORT = 12345
-    color = ['red', 'blue', 'green', 'yellow', 'white']
+    color = ['red', 'blue', 'green', 'yellow']
     key = 111
-    
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, PORT))
         server_socket.listen(4)
@@ -25,14 +25,31 @@ def main():
                 print('Connected to player', len(players), 'from', address)
                 player = cl.Player(len(players), client_socket)
                 players.append(player)
-                if len(players) == 3:
-                    start_time = time.time()   # when 3 players, start timer
-                elif len(players) == 4 or (start_time and time.time() - start_time > 60):  # if 4 players or 60 seconds passed, start game
-                    Game = cl.Game(players, color, key)
-                    Game.start_game()
-                    games.append(Game)
-                    players = []
-                    
+
+            if len(players) == 3 and start_time is None:
+                start_time = time.time()
+                print('start time:', start_time)
+                start_game_condition = False
+            elif len(players) == 3 and start_time is not None and (time.time() - start_time > 60) or len(players) == 4:
+              start_game_condition = True
+            else:
+              start_game_condition = False
+
+            if start_game_condition: 
+              Game = cl.Game(players, key)
+              Game.start_game(color)
+              games.append(Game)
+              players = []
+              start_time = None
+
+        server_socket.close()
+        games = []
+        players = []
+        start_time = None
+            
+
+
 if __name__ == "__main__":
     main()
+
                 
